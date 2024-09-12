d9504b1ba84499a39a467fd757b46c3b45c4be69
adcimon <37084114+adcimon@users.noreply.github.com>
2024-09-12 13:54:30 +0200

    Added name and surname validation

--- backend/src/validation/validation.schema.ts
+++ backend/src/validation/validation.schema.ts
@@ -2,16 +2,22 @@ import * as yup from 'yup';
 
 export namespace ValidationSchema {
 	const PHONE_REGEXP = /^(^$)|(\+[0-9]*)$/;
-	const PHONE_MESSAGE = 'Phone starts with a country prefix and contains numerical values.';
+	const PHONE_MESSAGE = 'Phones must start with a country prefix and contain numerical values.';
+
+	const NAME_REGEXP = /^[a-zA-Z][a-zA-Z0-9]+$/;
+	const NAME_MESSAGE = 'Names must start with an alpha character and contain alphanumeric characters.';
+
+	const SURNAME_REGEXP = /^([a-zA-Z][a-zA-Z0-9]+)?$/;
+	const SURNAME_MESSAGE = 'Surnames must start with an alpha character and contain alphanumeric characters.';
 
 	const AVATAR_REGEXP = /^(^$)|(([^]*)(\.)(jpg|jpeg|png))$/;
-	const AVATAR_MESSAGE = 'Avatar only supports jpg, jpeg and png files.';
+	const AVATAR_MESSAGE = 'Avatars only supports jpg, jpeg and png files.';
 
 	export const EmailSchema = yup.string().email();
 	export const PasswordSchema = yup.string();
 	export const PhoneSchema = yup.string().matches(PHONE_REGEXP, PHONE_MESSAGE);
-	export const NameSchema = yup.string();
-	export const SurnameSchema = yup.string();
+	export const NameSchema = yup.string().matches(NAME_REGEXP, NAME_MESSAGE);
+	export const SurnameSchema = yup.string().matches(SURNAME_REGEXP, SURNAME_MESSAGE);
 	export const BirthdateSchema = yup.string();
 	export const CountrySchema = yup.string();
 	export const TimezoneSchema = yup.string();
