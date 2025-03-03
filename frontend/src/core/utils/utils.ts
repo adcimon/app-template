a9928eaf4d0c3bfa33e978d9cb5c185163e71b4c
adcimon <37084114+adcimon@users.noreply.github.com>
2025-03-03 16:49:12 +0100

    Implemented download file function

--- frontend/src/core/utils/utils.ts
+++ frontend/src/core/utils/utils.ts
@@ -3,6 +3,17 @@ export namespace Utils {
 		navigator.clipboard.writeText(text);
 	};
 
+	export const downloadFile = (name: string, content: string) => {
+		const element: HTMLAnchorElement = document.createElement('a');
+		element.style.display = 'none';
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+		element.setAttribute('download', name);
+
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
+	};
+
 	export const readImage = async (file: File): Promise<string> => {
 		const promise: Promise<string> = new Promise((resolve, reject) => {
 			const reader: FileReader = new FileReader();
