f0de81df98ddab8b7fafe7f616b1e9be4e14327b
adcimon <37084114+adcimon@users.noreply.github.com>
2025-03-27 09:25:51 +0100

    Implemented link button

---
+++ frontend/src/core/components/Button/LinkButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import LaunchIcon from '@mui/icons-material/Launch';
+import Link from '@mui/material/Link';
+
+type LinkButtonProps = IconButtonProps & {
+	link?: string;
+};
+
+export const LinkButton: React.FC<LinkButtonProps> = (props: LinkButtonProps): JSX.Element => {
+	const render = () => {
+		return (
+			<>
+				<Link
+					href={props.link}
+					target='_blank'>
+					<IconButton {...props}>
+						<LaunchIcon
+							color='primary'
+							fontSize='inherit'
+						/>
+					</IconButton>
+				</Link>
+			</>
+		);
+	};
+
+	return render();
+};
