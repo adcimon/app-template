aa8f743f25853ded3675b678776bd4a72d7db813
adcimon <37084114+adcimon@users.noreply.github.com>
2025-04-17 18:38:09 +0200

    Minor change

--- frontend/src/core/components/Toggle/PinToggle.tsx
+++ frontend/src/core/components/Toggle/PinToggle.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { Checkbox, SxProps } from '@mui/material';
+import { Checkbox, CheckboxProps, SxProps } from '@mui/material';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 
-interface PinToggleProps {
+type PinToggleProps = CheckboxProps & {
 	pinned?: boolean;
-	size?: 'small' | 'medium' | 'large';
-	color?: string;
-	onChange?: (pinned: boolean) => void;
-}
+	iconColor?: string;
+	onChange?: (event: any, pinned: boolean) => void;
+};
 
-export const PinToggle = (props: PinToggleProps): React.JSX.Element => {
+export const PinToggle = ({ pinned, iconColor, onChange, ...props }: PinToggleProps): React.JSX.Element => {
 	const render = () => {
 		const iconSx: SxProps = {
-			color: props.color ?? 'text.primary',
+			color: iconColor ?? 'text.primary',
 		};
 		return (
 			<Checkbox
-				checked={props.pinned}
-				size={props.size}
+				{...props}
+				checked={pinned}
 				icon={<PushPinOutlinedIcon sx={iconSx} />}
 				checkedIcon={<PushPinIcon sx={iconSx} />}
-				onChange={(event: any, checked: boolean) => props.onChange?.(checked)}
+				onChange={(event: any, checked: boolean) => onChange?.(event, checked)}
 			/>
 		);
 	};
