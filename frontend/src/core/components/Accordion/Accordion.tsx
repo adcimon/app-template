8c464768b7a8adfa99c89a974602cafb92a11d13
adcimon <37084114+adcimon@users.noreply.github.com>
2025-04-22 16:33:34 +0200

    Minor changes

--- frontend/src/core/components/Accordion/Accordion.tsx
+++ frontend/src/core/components/Accordion/Accordion.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import * as MUI from '@mui/material';
-import { AccordionDetails, AccordionSummary, Divider, SxProps } from '@mui/material';
+import { AccordionDetails, AccordionSummary, Divider } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-interface AccordionProps {
+type AccordionProps = MUI.AccordionProps & {
 	header?: React.ReactNode;
-	defaultExpanded?: boolean;
-	expanded?: boolean;
-	onChange?: (expanded: boolean) => void;
-	sx?: SxProps;
-	children?: React.ReactNode;
-}
+};
 
-export const Accordion = (props: AccordionProps): React.JSX.Element => {
+export const Accordion = ({ header, ...props }: AccordionProps): React.JSX.Element => {
 	const render = () => {
 		return (
-			<MUI.Accordion
-				defaultExpanded={props.defaultExpanded}
-				expanded={props.expanded}
-				onChange={(event: any, expanded: boolean) => props.onChange?.(expanded)}
-				sx={props.sx}>
-				<AccordionSummary expandIcon={<ArrowDropDownIcon />}>{props.header}</AccordionSummary>
+			<MUI.Accordion {...props}>
+				<AccordionSummary expandIcon={<ArrowDropDownIcon />}>{header}</AccordionSummary>
 				<Divider />
 				<AccordionDetails
 					sx={{
