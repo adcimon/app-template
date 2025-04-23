fa7c5a5cb84fe648de5f883f26a38e14e5c750ce
adcimon <37084114+adcimon@users.noreply.github.com>
2025-04-23 16:32:43 +0200

    Minor changes

--- frontend/src/components/Dialog/PrivacyPolicyDialog.tsx
+++ frontend/src/components/Dialog/PrivacyPolicyDialog.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Link } from '@mui/material';
+import { useAppState } from '../../states/app/useAppState';
 
 interface PrivacyPolicyDialogProps {
 	open: boolean;
@@ -7,6 +8,8 @@ interface PrivacyPolicyDialogProps {
 }
 
 export const PrivacyPolicyDialog = (props: PrivacyPolicyDialogProps): React.JSX.Element => {
+	const appState = useAppState();
+
 	const render = () => {
 		return (
 			<Dialog
@@ -15,51 +18,76 @@ export const PrivacyPolicyDialog = (props: PrivacyPolicyDialogProps): React.JSX.
 				scroll='paper'>
 				<DialogTitle>Privacy Policy</DialogTitle>
 				<DialogContent dividers={true}>
-					<DialogContentText tabIndex={-1}>
-						Thank you for visiting our website. This Privacy Policy explains how we collect, use, and
-						protect your personal information when you interact with our website. Please take a moment to
-						read this policy carefully.
+					<DialogContentText
+						component='div'
+						tabIndex={-1}
+						sx={{
+							textAlign: 'justify',
+						}}>
+						Thank you for visiting {appState.metadata?.name}. This Privacy Policy explains how we collect,
+						use, and protect your personal information when you interact with our website. Please take a
+						moment to read this policy carefully.
 						<br />
-						1. Information We Collect We may collect certain personally identifiable information when you
-						visit our website, such as your name, email address, and any other information you provide
-						voluntarily. Additionally, we may collect non-personal information such as your IP address,
-						browser type, and operating system for analytical purposes.
 						<br />
-						2. Use of Information We use the collected information to provide you with a personalized
-						experience on our website, respond to your inquiries, and improve our services. We may also use
-						the information to send you promotional emails or newsletters, but you will always have the
-						option to opt-out of such communications.
+						<strong>1. Information.</strong>
 						<br />
-						3. Cookies Our website uses cookies to enhance your browsing experience. Cookies are small files
-						stored on your device that help us analyze website traffic and improve our website's
-						functionality. By using our website, you consent to the use of cookies.
+						We may collect certain personally identifiable information when you visit our website, such as
+						your name, email address, and any other information you provide voluntarily. Additionally, we
+						may collect non-personal information such as your IP address, browser type, and operating system
+						for analytical purposes.
 						<br />
-						4. Data Security We prioritize the security of your personal information and employ
-						industry-standard security measures to protect it against unauthorized access, loss, or
-						alteration. However, please be aware that no method of transmission over the internet or
-						electronic storage is 100% secure, and we cannot guarantee absolute security.
 						<br />
-						5. Third-Party Links Our website may contain links to third-party websites or services. Please
-						note that we are not responsible for the privacy practices or the content of such third-party
-						websites. We encourage you to review the privacy policies of those websites before providing any
-						personal information.
+						<strong>2. Use of Information.</strong>
 						<br />
-						6. Children's Privacy Our website is not intended for children under the age of 13. We do not
-						knowingly collect or store personal information from children. If you believe that we have
-						inadvertently collected personal information from a child, please contact us immediately, and we
-						will take steps to delete the information.
+						We use the collected information to provide you with a personalized experience on our website,
+						respond to your inquiries, and improve our services. We may also use the information to send you
+						promotional emails or newsletters, but you will always have the option to opt-out of such
+						communications.
 						<br />
-						7. Changes to the Privacy Policy We reserve the right to update or modify this Privacy Policy at
-						any time without prior notice. Any changes will be effective immediately upon posting the
-						revised policy on our website. Your continued use of the website after the changes are made will
-						indicate your acceptance of the revised policy.
 						<br />
-						8. Contact Us If you have any questions or concerns regarding this Privacy Policy or our privacy
-						practices, please contact us at [your contact information].
+						<strong>3. Cookies.</strong>
 						<br />
-						Please note that this is a temporary placeholder privacy policy. We recommend consulting with a
-						legal professional to tailor a privacy policy that aligns with your specific website and
-						applicable laws and regulations.
+						Our website uses cookies to enhance your browsing experience. Cookies are small files stored on
+						your device that help us analyze website traffic and improve our website's functionality. By
+						using our website, you consent to the use of cookies.
+						<br />
+						<br />
+						<strong>4. Data Security.</strong>
+						<br />
+						We prioritize the security of your personal information and employ industry-standard security
+						measures to protect it against unauthorized access, loss, or alteration. However, please be
+						aware that no method of transmission over the internet or electronic storage is 100% secure, and
+						we cannot guarantee absolute security.
+						<br />
+						<br />
+						<strong>5. Third-Party Links.</strong>
+						<br />
+						Our website may contain links to third-party websites or services. Please note that we are not
+						responsible for the privacy practices or the content of such third-party websites. We encourage
+						you to review the privacy policies of those websites before providing any personal information.
+						<br />
+						<br />
+						<strong>6. Children's Privacy.</strong>
+						<br />
+						Our website is not intended for children under the age of 13. We do not knowingly collect or
+						store personal information from children. If you believe that we have inadvertently collected
+						personal information from a child, please contact us immediately, and we will take steps to
+						delete the information.
+						<br />
+						<br />
+						<strong>7. Changes to the Privacy Policy.</strong>
+						<br />
+						We reserve the right to update or modify this Privacy Policy at any time without prior notice.
+						Any changes will be effective immediately upon posting the revised policy on our website. Your
+						continued use of the website after the changes are made will indicate your acceptance of the
+						revised policy.
+						<br />
+						<br />
+						<strong>8. Contact Information.</strong>
+						<br />
+						If you have any questions or concerns regarding this Privacy Policy or our privacy practices,
+						please contact us at <Link>{appState.metadata?.email}</Link>.
+						<br />
 						<br />
 					</DialogContentText>
 				</DialogContent>
--- frontend/src/components/Dialog/TermsOfServiceDialog.tsx
+++ frontend/src/components/Dialog/TermsOfServiceDialog.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Link } from '@mui/material';
+import { useAppState } from '../../states/app/useAppState';
 
 interface TermsOfServiceDialogProps {
 	open: boolean;
@@ -7,6 +8,8 @@ interface TermsOfServiceDialogProps {
 }
 
 export const TermsOfServiceDialog = (props: TermsOfServiceDialogProps): React.JSX.Element => {
+	const appState = useAppState();
+
 	const render = () => {
 		return (
 			<Dialog
@@ -15,58 +18,116 @@ export const TermsOfServiceDialog = (props: TermsOfServiceDialogProps): React.JS
 				scroll='paper'>
 				<DialogTitle>Terms of Service</DialogTitle>
 				<DialogContent dividers={true}>
-					<DialogContentText tabIndex={-1}>
-						Effective Date: [Insert Effective Date]
+					<DialogContentText
+						component='div'
+						tabIndex={-1}
+						sx={{
+							textAlign: 'justify',
+						}}>
+						Effective Date: {appState.metadata?.date}
 						<br />
-						1. Introduction Welcome to [Your Website/App Name] ("the Service"). These Terms of Service
-						govern your use of the Service. By accessing or using the Service, you agree to be bound by
-						these Terms of Service. If you do not agree with these terms, you should not use the Service.
 						<br />
-						2. Description of Service [Describe the services or features your website/app offers]
+						<strong>1. Introduction.</strong>
 						<br />
-						3. Acceptance of Terms By accessing or using the Service, you acknowledge that you have read,
-						understood, and agree to be bound by these Terms of Service. If you are using the Service on
-						behalf of an organization, you represent and warrant that you have the authority to enter into
-						these terms on behalf of the organization.
+						Welcome to {appState.metadata?.name} ("the Service"). These Terms of Service govern your use of
+						the Service. By accessing or using the Service, you agree to be bound by these Terms of Service.
+						If you do not agree with these terms, you should not use the Service.
 						<br />
-						4. User Conduct You agree to use the Service only for lawful purposes and in accordance with
-						these Terms of Service. You are responsible for any content you submit or post on the Service.
-						You must not: - [List prohibited activities, such as unauthorized access, spamming, etc.] -
-						[Specify any content restrictions, if applicable]
 						<br />
-						5. Intellectual Property [Specify ownership of content and intellectual property rights] [If
-						applicable, explain that users retain ownership of their content but grant the Service a license
-						to use it]
+						<strong>2. Description of Service.</strong>
 						<br />
-						6. Third-Party Links and Content The Service may contain links to third-party websites or
-						content. We do not endorse or control the content on these third-party sites and are not
-						responsible for any actions or omissions of third parties. Your use of third-party websites is
-						at your own risk.
+						{appState.metadata?.description}
 						<br />
-						7. Disclaimer of Warranties The Service is provided on an "as is" and "as available" basis
-						without any warranties or representations of any kind. We do not warrant that the Service will
-						be error-free, uninterrupted, or free of viruses or other harmful components.
 						<br />
-						8. Limitation of Liability In no event shall [Your Company Name] or its affiliates be liable for
-						any direct, indirect, incidental, special, or consequential damages arising out of or in any way
-						connected with your use of the Service. You agree to indemnify and hold [Your Company Name]
+						<strong>3. Acceptance of Terms.</strong>
+						<br />
+						By accessing or using the Service, you acknowledge that you have read, understood, and agree to
+						be bound by these Terms of Service. If you are using the Service on behalf of an organization,
+						you represent and warrant that you have the authority to enter into these terms on behalf of the
+						organization.
+						<br />
+						<br />
+						<strong>4. User Conduct.</strong>
+						<br />
+						You agree to use the Service only for lawful purposes and in accordance with these Terms of
+						Service. You are responsible for any content you submit or post on the Service. You must not:
+						<ul>
+							<li>Violate any laws or regulations.</li>
+							<li>Access or interfere with the Service or its systems without authorization.</li>
+							<li>Upload harmful software or engage in disruptive behavior.</li>
+							<li>Harass, impersonate, or harm others.</li>
+							<li>Post illegal, offensive, or infringing content.</li>
+							<li>Spam or use the Service for unauthorized advertising.</li>
+							<li>Record, distribute, or misuse live content without permission.</li>
+						</ul>
+						<br />
+						<strong>5. Intellectual Property.</strong>
+						<br />
+						All content provided by the Service, including software, design, and media, is owned by{' '}
+						{appState.metadata?.company?.name} or its licensors and is protected by intellectual property
+						laws. You retain ownership of any content you create and share on the platform, but by using the
+						Service, you grant us a license to use, display, and distribute that content as needed to
+						operate and promote the Service. You confirm that you have the rights to share your content and
+						that it doesn't infringe on anyone else's rights.
+						<br />
+						<br />
+						<strong>6. Third-Party Links and Content.</strong>
+						<br />
+						The Service may contain links to third-party websites or content. We do not endorse or control
+						the content on these third-party sites and are not responsible for any actions or omissions of
+						third parties. Your use of third-party websites is at your own risk.
+						<br />
+						<br />
+						<strong>7. Disclaimer of Warranties.</strong>
+						<br />
+						The Service is provided on an "as is" and "as available" basis without any warranties or
+						representations of any kind. We do not warrant that the Service will be error-free,
+						uninterrupted, or free of viruses or other harmful components.
+						<br />
+						<br />
+						<strong>8. Limitation of Liability.</strong>
+						<br />
+						In no event shall {appState.metadata?.company?.name} or its affiliates be liable for any direct,
+						indirect, incidental, special, or consequential damages arising out of or in any way connected
+						with your use of the Service. You agree to indemnify and hold {appState.metadata?.company?.name}{' '}
 						harmless from any claims or liabilities arising from your use of the Service.
 						<br />
-						9. Termination We reserve the right to terminate your access to the Service at any time, with or
-						without cause or notice. You may also terminate your use of the Service at any time.
 						<br />
-						10. Governing Law and Jurisdiction These Terms of Service shall be governed by and construed in
-						accordance with the laws of [Your Jurisdiction]. Any disputes arising out of or in connection
-						with these terms shall be resolved exclusively by the courts of [Your Jurisdiction].
+						<strong>9. Termination.</strong>
 						<br />
-						11. Changes to the Terms We may update or modify these Terms of Service from time to time. Any
-						changes will be effective immediately upon posting the revised terms on the Service. By
-						continuing to use the Service after such changes, you accept and agree to the revised terms.
+						We reserve the right to terminate your access to the Service at any time, with or without cause
+						or notice. You may also terminate your use of the Service at any time.
 						<br />
-						12. Contact Information If you have any questions or concerns about these Terms of Service,
-						please contact us at [Your Contact Email].
 						<br />
-						[Your Company Name] [Your Address] [Your Website URL]
+						<strong>10. Governing Law and Jurisdiction.</strong>
+						<br />
+						These Terms of Service shall be governed by and construed in accordance with the laws of{' '}
+						{appState.metadata?.company?.country}. Any disputes arising out of or in connection with these
+						terms shall be resolved exclusively by the courts of {appState.metadata?.company?.country}.
+						<br />
+						<br />
+						<strong>11. Changes to the Terms.</strong>
+						<br />
+						We may update or modify these Terms of Service from time to time. Any changes will be effective
+						immediately upon posting the revised terms on the Service. By continuing to use the Service
+						after such changes, you accept and agree to the revised terms.
+						<br />
+						<br />
+						<strong>12. Contact Information.</strong>
+						<br />
+						If you have any questions or concerns about these Terms of Service, please contact us at{' '}
+						<Link>{appState.metadata?.email}</Link>.
+						<br />
+						<br />
+						{appState.metadata?.company?.name}
+						<br />
+						{appState.metadata?.company?.address}
+						<br />
+						<Link
+							href={appState.metadata?.company?.url}
+							target='_blank'>
+							{appState.metadata?.company?.url}
+						</Link>
 					</DialogContentText>
 				</DialogContent>
 				<DialogActions>{props.onClose && <Button onClick={props.onClose}>Accept</Button>}</DialogActions>
