80ed6031e51c092b978b47fe43270fbdc8cd51a9
adcimon <37084114+adcimon@users.noreply.github.com>
2025-06-18 16:06:51 +0200

    Minor change

--- frontend/src/components/Windows/SettingsWindow/PreferencesMenu/PreferencesMenu.tsx
+++ frontend/src/components/Windows/SettingsWindow/PreferencesMenu/PreferencesMenu.tsx
@@ -17,6 +17,51 @@ export const PreferencesMenu = (): React.JSX.Element => {
 		appState.setTheme(theme);
 	};
 
+	const renderTheme = () => {
+		return (
+			<>
+				<Box>
+					<Typography>Theme</Typography>
+					<Typography
+						variant='body2'
+						sx={{
+							color: 'text.secondary',
+							marginTop: '5px',
+						}}>
+						Change the appearance of the application.
+					</Typography>
+				</Box>
+				<Select
+					value={appState.theme.toString()}
+					onChange={handleChangeTheme}>
+					<MenuItem value={0}>Base</MenuItem>
+					<MenuItem value={1}>Custom Light</MenuItem>
+					<MenuItem value={2}>Custom Dark</MenuItem>
+				</Select>
+			</>
+		);
+	};
+
+	const renderItem = (children: React.ReactNode) => {
+		return (
+			<Grid2
+				size={{
+					xs: 12,
+					sm: 12,
+					md: 12,
+				}}>
+				<Box
+					sx={{
+						alignItems: 'center',
+						display: 'flex',
+						justifyContent: 'space-between',
+					}}>
+					{children}
+				</Box>
+			</Grid2>
+		);
+	};
+
 	const render = () => {
 		return (
 			<Card>
@@ -33,38 +78,7 @@ export const PreferencesMenu = (): React.JSX.Element => {
 						<Grid2
 							container
 							spacing={3}>
-							<Grid2
-								size={{
-									xs: 12,
-									sm: 12,
-									md: 12,
-								}}>
-								<Box
-									sx={{
-										alignItems: 'center',
-										display: 'flex',
-										justifyContent: 'space-between',
-									}}>
-									<Box>
-										<Typography>Theme</Typography>
-										<Typography
-											variant='body2'
-											sx={{
-												color: 'text.secondary',
-												marginTop: '5px',
-											}}>
-											Change the appearance of the application.
-										</Typography>
-									</Box>
-									<Select
-										value={appState.theme.toString()}
-										onChange={handleChangeTheme}>
-										<MenuItem value={0}>Base</MenuItem>
-										<MenuItem value={1}>Custom Light</MenuItem>
-										<MenuItem value={2}>Custom Dark</MenuItem>
-									</Select>
-								</Box>
-							</Grid2>
+							{renderItem(renderTheme())}
 						</Grid2>
 					</Box>
 				</CardContent>
