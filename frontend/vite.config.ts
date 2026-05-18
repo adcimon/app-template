3dd9394cdba4b597c3e8ce65b87b8d9ea27551d2
adcimon <37084114+adcimon@users.noreply.github.com>
2026-05-18 12:29:55 +0200

    Added vite app version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 9001,
+	},
+	define: {
+		__APP_VERSION__: JSON.stringify(packageJson.version),
 	},
 });
